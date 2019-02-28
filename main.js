af632e7395b55dbb0e149c98ec98c6e8050c925e
Alex drinkwater <aldrinkwater@hotmail.com>
2019-02-28 19:08:50 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const baseURL = "https://swapi.co/api/";
+
+function getData(type, cb) {
+
+    var xhr = new XMLHttpRequest(); //create new instance of XMLHttpRequest object
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+
+        }
+    };
+
+}
+
+function getTableHeaders(obj){
+    var tableHeaders = [];
+    
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML =   "";
+    getData(type, function(data){
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+        getTableHeaders(data[0]);
+        
+        data.forEach(function(item){
+         var dataRow = [];
+         
+         Object.keys(item).forEach(function(key){
+             var rowData = item[key].toString();
+             var truncateData = rowData.substring(0, 15);
+             dataRow.push(`<td>${truncateData}</td>`);
+         });
+         tableRows.push(`<tr>${dataRow}</tr>`);
+
+        });
+el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
