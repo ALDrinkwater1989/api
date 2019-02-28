a93c316fff58081f04d0cded2f68a74bed933b77
Alex drinkwater <aldrinkwater@hotmail.com>
2019-02-28 19:24:56 +0000

    fixed comma visual bug

--- main.js
+++ main.js
@@ -61,6 +61,6 @@ function writeToDocument(url) {
             tableRows.push(`<tr>${dataRow}</tr>`);
 
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
